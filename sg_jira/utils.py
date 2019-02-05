21dbe8f0f720a207e3cda0ff7fb525126b4c188f
Stephane Deverly <stephane.deverly@gpltech.com>
2019-02-05 16:55:38 +0100

    For 605, utf8_encode and tests

--- sg_jira/utils.py
+++ sg_jira/utils.py
@@ -10,15 +10,19 @@ def utf8_decode(value):
     """
     Convert any string in the given value to an utf8 decoded unicode value.
 
-    Recursively treat containers by iterating on all the values they contain.
+    Treat containers by recursively iterating over all the values they contain.
 
     :param value: A string, a list or a dictionary.
-    :raises: ValueError if an utf8 decoded key is already present in the original
-             value of a dictionary.
+    :raises: ValueError if a converted utf8 decoded key is already present in the
+             original value of a dictionary.
     """
     if isinstance(value, list):
         # Convert all values
         return [utf8_decode(x) for x in value]
+
+    if isinstance(value, tuple):
+        # Convert all values
+        return tuple([utf8_decode(x) for x in value])
 
     if isinstance(value, dict):
         # Convert the keys and the values
@@ -31,7 +35,7 @@ def utf8_decode(value):
                 decoded_key = k.decode("utf-8")
                 if decoded_key in [x for x in value.keys() if isinstance(x, unicode)]:
                     raise ValueError(
-                        "Utf8 decoded key for %s is already present "
+                        "Utf8 decoded key %s is already present "
                         "in dictionary being decoded" % (
                             decoded_key,
                         )
@@ -43,6 +47,57 @@ def utf8_decode(value):
 
     if isinstance(value, str):
         return value.decode("utf-8")
+
+    # Nothing to do, return the value unchanged.
+    return value
+
+
+def utf8_encode(value):
+    """
+    Convert any unicode in the given value to an utf8 encoded string value.
+
+    Treat containers by recursively iterating over all the values they contain.
+
+    :param value: A string, a list or a dictionary.
+    :raises: ValueError if a converted utf8 encoded key is already present in the
+             original value of a dictionary.
+    """
+    if isinstance(value, list):
+        # Convert all values
+        return [utf8_encode(x) for x in value]
+
+    if isinstance(value, tuple):
+        # Convert all values
+        return tuple([utf8_encode(x) for x in value])
+
+    if isinstance(value, dict):
+        # Convert the keys and the values
+        encoded = {}
+        for k, v in value.iteritems():
+            # We need to check if there is a potential conflict between the
+            # encoded key and an existing str key, so we can't blindly call
+            # ourself here.
+            if isinstance(k, unicode):
+                encoded_key = k.encode("utf-8")
+                if encoded_key in [x for x in value.keys() if isinstance(x, str)]:
+                    # Note: we issue the error with the unicode value to be
+                    # consistent with our convention where everything is unicode
+                    # and don't get potential problems with an utf-8 encoded string
+                    # being used with unicode values, which would potentislly lead
+                    # to UnicodeDecode errors.
+                    raise ValueError(
+                        "Utf8 encoded key for %s is already present "
+                        "in dictionary being encoded" % (
+                            k,
+                        )
+                    )
+            else:
+                encoded_key = k
+            encoded[encoded_key] = utf8_encode(v)
+        return encoded
+
+    if isinstance(value, unicode):
+        return value.encode("utf-8")
 
     # Nothing to do, return the value unchanged.
     return value
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -37,7 +37,7 @@ class TestUtils(TestBase):
         res = sg_jira.utf8_decode([UTF8_ENCODED_STRING, UNICODE_STRING, UTF8_ENCODED_STRING])
         self.assertEqual(res, [UNICODE_STRING, UNICODE_STRING, UNICODE_STRING])
 
-        # All keys and values in a dict should be decode if needed
+        # All keys and values in a dict should be decoded if needed
         res = sg_jira.utf8_decode({
             UTF8_ENCODED_STRING: UTF8_ENCODED_STRING,
             "foo": UNICODE_STRING,
@@ -66,7 +66,7 @@ class TestUtils(TestBase):
             })
         self.assertIsNotNone(
             re.match(
-                "Utf8 decoded key for %s is already present in dictionary being decoded" % UNICODE_STRING,
+                "Utf8 decoded key %s is already present in dictionary being decoded" % UNICODE_STRING,
                 cm.exception.message,
             )
         )
@@ -104,10 +104,10 @@ class TestUtils(TestBase):
             res,
             [decoded] * 7
         )
-        # Nested dictionaries and lists
+        # Nested dictionaries lists and tuples
         d = {
             UTF8_ENCODED_STRING: UTF8_ENCODED_STRING,
-            "foo": [UTF8_ENCODED_STRING, UNICODE_STRING, UTF8_ENCODED_STRING],
+            "foo": (UTF8_ENCODED_STRING, UNICODE_STRING, UTF8_ENCODED_STRING),
             "blah": [1, 2, 3, 4],
             "%s_bis" % UNICODE_STRING: [
                 UTF8_ENCODED_STRING,
@@ -123,7 +123,7 @@ class TestUtils(TestBase):
         res = sg_jira.utf8_decode(d)
         decoded = {
             UNICODE_STRING: UNICODE_STRING,
-            "foo": [UNICODE_STRING, UNICODE_STRING, UNICODE_STRING],
+            "foo": (UNICODE_STRING, UNICODE_STRING, UNICODE_STRING),
             "blah": [1, 2, 3, 4],
             "%s_bis" % UNICODE_STRING: [
                 UNICODE_STRING,
@@ -141,3 +141,123 @@ class TestUtils(TestBase):
             decoded
         )
 
+    def test_utf8_encode(self):
+        """
+        Test utf8 encoding.
+        """
+        # string value should be unchanged
+        res = sg_jira.utf8_encode(UTF8_ENCODED_STRING)
+        self.assertEqual(res, UTF8_ENCODED_STRING)
+
+        # Utf-8 encoded string should be decoded
+        res = sg_jira.utf8_encode(UNICODE_STRING)
+        self.assertEqual(res, UTF8_ENCODED_STRING)
+
+        # All values in a list should be decode if needed
+        res = sg_jira.utf8_encode([UTF8_ENCODED_STRING, UNICODE_STRING, UTF8_ENCODED_STRING])
+        self.assertEqual(res, [UTF8_ENCODED_STRING, UTF8_ENCODED_STRING, UTF8_ENCODED_STRING])
+
+        # All keys and values in a dict should be encoded if needed
+        res = sg_jira.utf8_encode({
+            UNICODE_STRING: UNICODE_STRING,
+            "foo": UTF8_ENCODED_STRING,
+            "blah": 1,
+            "%s_bis" % UNICODE_STRING: UTF8_ENCODED_STRING,
+        })
+        self.assertEqual(
+            res,
+            {
+                UTF8_ENCODED_STRING: UTF8_ENCODED_STRING,
+                "foo": UTF8_ENCODED_STRING,
+                "blah": 1,
+                "%s_bis" % UTF8_ENCODED_STRING: UTF8_ENCODED_STRING,
+            }
+        )
+        # Check that conflicts between a decoded key and an existing one are
+        # correctly detected.
+        with self.assertRaises(ValueError) as cm:
+            # Note: we can't use self.assertRaisesRegexp which calls `str` on
+            # the error message to perform the match, and fails with UnicodeEncodeError.
+            sg_jira.utf8_encode({
+                UTF8_ENCODED_STRING: UTF8_ENCODED_STRING,
+                "foo": UNICODE_STRING,
+                "blah": 1,
+                UNICODE_STRING: UTF8_ENCODED_STRING,
+            })
+        self.assertIsNotNone(
+            re.match(
+                "Utf8 encoded key for %s is already present in dictionary being encoded" % UNICODE_STRING,
+                cm.exception.message,
+            )
+        )
+
+        # A dictionary with lists
+        res = sg_jira.utf8_encode({
+            UNICODE_STRING: UNICODE_STRING,
+            "foo": [UTF8_ENCODED_STRING, UNICODE_STRING, UTF8_ENCODED_STRING],
+            "blah": [1, 2, 3, 4],
+            "%s_bis" % UTF8_ENCODED_STRING: [UTF8_ENCODED_STRING, UNICODE_STRING, UTF8_ENCODED_STRING],
+        })
+        self.assertEqual(
+            res,
+            {
+                UTF8_ENCODED_STRING: UTF8_ENCODED_STRING,
+                "foo": [UTF8_ENCODED_STRING, UTF8_ENCODED_STRING, UTF8_ENCODED_STRING],
+                "blah": [1, 2, 3, 4],
+                "%s_bis" % UTF8_ENCODED_STRING: [UTF8_ENCODED_STRING, UTF8_ENCODED_STRING, UTF8_ENCODED_STRING],
+            }
+        )
+        # A list of dictionaries
+        d = {
+            UNICODE_STRING: UNICODE_STRING,
+            "foo": [UTF8_ENCODED_STRING, UNICODE_STRING, UTF8_ENCODED_STRING],
+            "blah": [1, 2, 3, 4],
+            "%s_bis" % UTF8_ENCODED_STRING: [UTF8_ENCODED_STRING, UNICODE_STRING, UTF8_ENCODED_STRING],
+        }
+        res = sg_jira.utf8_encode([d] * 7)
+        encoded = {
+            UTF8_ENCODED_STRING: UTF8_ENCODED_STRING,
+            "foo": [UTF8_ENCODED_STRING, UTF8_ENCODED_STRING, UTF8_ENCODED_STRING],
+            "blah": [1, 2, 3, 4],
+            "%s_bis" % UTF8_ENCODED_STRING: [UTF8_ENCODED_STRING, UTF8_ENCODED_STRING, UTF8_ENCODED_STRING],
+        }
+        self.assertEqual(
+            res,
+            [encoded] * 7
+        )
+        # Nested dictionaries lists and tuples
+        d = {
+            UNICODE_STRING: UNICODE_STRING,
+            "foo": (UTF8_ENCODED_STRING, UNICODE_STRING, UTF8_ENCODED_STRING),
+            "blah": [1, 2, 3, 4],
+            "%s_bis" % UTF8_ENCODED_STRING: [
+                UNICODE_STRING,
+                {
+                    UNICODE_STRING: UNICODE_STRING,
+                    "foo": [UTF8_ENCODED_STRING, UNICODE_STRING, UTF8_ENCODED_STRING],
+                    "blah": [1, 2, 3, 4],
+                    "%s_bis" % UTF8_ENCODED_STRING: [UTF8_ENCODED_STRING, UNICODE_STRING, UTF8_ENCODED_STRING],
+                },
+                UNICODE_STRING, UTF8_ENCODED_STRING
+            ],
+        }
+        res = sg_jira.utf8_encode(d)
+        encoded = {
+            UTF8_ENCODED_STRING: UTF8_ENCODED_STRING,
+            "foo": (UTF8_ENCODED_STRING, UTF8_ENCODED_STRING, UTF8_ENCODED_STRING),
+            "blah": [1, 2, 3, 4],
+            "%s_bis" % UTF8_ENCODED_STRING: [
+                UTF8_ENCODED_STRING,
+                {
+                    UTF8_ENCODED_STRING: UTF8_ENCODED_STRING,
+                    "foo": [UTF8_ENCODED_STRING, UTF8_ENCODED_STRING, UTF8_ENCODED_STRING],
+                    "blah": [1, 2, 3, 4],
+                    "%s_bis" % UTF8_ENCODED_STRING: [UTF8_ENCODED_STRING, UTF8_ENCODED_STRING, UTF8_ENCODED_STRING],
+                },
+                UTF8_ENCODED_STRING, UTF8_ENCODED_STRING
+            ],
+        }
+        self.assertEqual(
+            res,
+            encoded
+        )
