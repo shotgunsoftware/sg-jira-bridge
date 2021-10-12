d0602bcbb120b01853bb3bb24c496d356ee53908
Jean-François Boismenu <jfboismenu@users.noreply.github.com>
2021-10-12 10:14:15 -0400

    Warn missing lib (#52)
    Warns the user when one of the dependencies of the bridge is missing.

--- sg_jira/__init__.py
+++ sg_jira/__init__.py
@@ -5,6 +5,19 @@
 # this software in either electronic or hard copy form.
 #
 
+import sys
+
+if sys.version_info[0] == 2:
+    ex_type = ImportError
+else:
+    ex_type = ModuleNotFoundError
+
+for module_name in ["daemonize", "shotgun_api3", "jira", "dotenv", "six"]:
+    try:
+        __import__(module_name)
+    except ex_type as e:
+        raise RuntimeError("Could not import '%s' module. Did you install the requirements.txt file? Original error: %s" % (module_name, str(e)))
+
 from .bridge import Bridge
 from .syncer import Syncer
 from .jira_session import JiraSession
