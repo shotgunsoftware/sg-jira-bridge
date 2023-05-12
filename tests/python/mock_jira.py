c74743408d96d9284c1bed39cb8a538a27fa6208
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2023-05-12 13:23:59 -0500

    Update mocks to fix existing test

--- tests/python/mock_jira.py
+++ tests/python/mock_jira.py
@@ -1642,7 +1642,7 @@ class MockedJira(object):
         return ""
 
     def search_assignable_users_for_issues(
-        self, username=None, query=None, startAt=0, maxResults=20, *args, **kwargs
+        self, name, startAt=0, maxResults=20, *args, **kwargs
     ):
         """
         Mocked Jira method.
@@ -1657,11 +1657,13 @@ class MockedJira(object):
         elif query == JIRA_USER["emailAddress"]:
             return [User(options, None, JIRA_USER)]
 
-        elif query == JIRA_USER_2["emailAddress"]:
-            return [User(options, None, JIRA_USER_2)]
-        
-        else:
-            return []
+        # Create a list of users.
+        users = [User(options, None, JIRA_USER_2)] * maxResults + [
+            User(options, None, JIRA_USER)
+        ]
+
+        # Return the requested slice.
+        return users[startAt : startAt + maxResults]
 
     def user(self, id, payload="username"):
         """
