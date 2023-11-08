b4c33cf080d58f5b215a5dca4ae962fe5b68b412
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2023-11-08 12:29:28 -0500

    Auto-updated documentation based on branch/tag v0.4.0b4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.4.0b3',
+    VERSION: 'v0.4.0b4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
