8f197f352b018e4b759ae1a8166c02ae51cda5bb
Barbara Laigneau <barbara@dark-shot.com>
2024-03-21 17:14:01 +0100

    modify the method to build the field mapping

--- sg_jira/jira_session.py
+++ sg_jira/jira_session.py
@@ -95,11 +95,10 @@ class JiraSession(jira.client.JIRA):
         """
         # Build a mapping from Jira field names to their id for fast lookup.
         for jira_field in self.fields():
-            if self._name_is_shotgun_field(jira_field["name"]):
-                field_name = jira_field["name"].lower()
-            else:
-                field_name = jira_field["key"].lower()
-            self._jira_fields_map[field_name] = jira_field["id"]
+            # add both the name and key to the mapping to manage all the different use cases
+            self._jira_fields_map[jira_field["name"].lower()] = jira_field["id"]
+            self._jira_fields_map[jira_field["key"].lower()] = jira_field["id"]
+
         self._jira_shotgun_type_field = self.get_jira_issue_field_id(
             JIRA_SHOTGUN_TYPE_FIELD.lower()
         )
@@ -121,13 +120,6 @@ class JiraSession(jira.client.JIRA):
             raise RuntimeError(
                 "Missing required custom Jira field %s" % JIRA_SHOTGUN_URL_FIELD
             )
-
-    def _name_is_shotgun_field(self, field_name):
-        return field_name.lower() in [
-            JIRA_SHOTGUN_TYPE_FIELD.lower(),
-            JIRA_SHOTGUN_ID_FIELD.lower(),
-            JIRA_SHOTGUN_URL_FIELD.lower(),
-        ]
 
     @property
     def is_jira_cloud(self):
